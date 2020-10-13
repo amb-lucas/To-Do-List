4972376be5d47bc9aebbb33de4fdcb9401f9bbb9
unknown <lucas.ambrosio.am@gmail.com>
2020-10-13 19:28:44 -0300

    Updates

---
+++ precache-manifest.fab93881237b07cbc53c7d8478dd6b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806b69d9ec47e1414626909dbd915cf6",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "305cfb45fddcb562dd1e",
+    "url": "/To-Do-List/static/css/main.fc6406e8.chunk.css"
+  },
+  {
+    "revision": "ce4299c67d700efd2feb",
+    "url": "/To-Do-List/static/js/2.c0560cc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List/static/js/2.c0560cc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "305cfb45fddcb562dd1e",
+    "url": "/To-Do-List/static/js/main.5baf7120.chunk.js"
+  },
+  {
+    "revision": "e71b0fca7b414be357cd",
+    "url": "/To-Do-List/static/js/runtime-main.8e2d492d.js"
+  }
+]);
